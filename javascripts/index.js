4f54c49d923fc8bf149e495b804b7e9541a6fc52
rhday <54642787+rhday@users.noreply.github.com>
2021-01-19 13:51:36 +0000

    built 'renderAccount'

--- javascripts/index.js
+++ javascripts/index.js
@@ -14,3 +14,23 @@ const loadAccounts = () => {
     })
 }
 
+const renderAccount = (accountHash) => {
+    const div = document.createElement("div")
+    const p = document.createElement("p")
+    const button = document.createElement("button")
+    const ul = document.createElement("ul")
+
+    div.setAttribute("class", "card")
+    div.setAttribute("data-id", accountHash.id)
+    p.innerHTML = accountHash.name
+    button.setAttribute("data-account-id", accountHash.id)
+    button.innerHTML = "Add Transaction"
+    //button.addEventListener("click", createTransaction)
+
+    div.appendChild(p)
+    div.appendChild(button)
+    div.appendChild(ul)
+
+    main.appendChild(div)
+    accountHash.transactions.forEach(transaction => renderTransaction(transaction))
+}
