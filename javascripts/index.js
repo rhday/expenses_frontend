57a323b62b5431f4215131ba1bdc8bdde1e2010a
rhday <54642787+rhday@users.noreply.github.com>
2021-01-19 13:27:25 +0000

    loadAccounts built

--- javascripts/index.js
+++ javascripts/index.js
@@ -4,3 +4,13 @@ const TRANSACTION_URL = `${BASE_URL}/api/v1/transactions`
 const main = document.querySelector("main")
 
 document.addEventListener("DOMContentLoaded", () => loadAccounts())
+
+const loadAccounts = () => {
+    const ACCOUNT_URL = `${BASE_URL}/api/v1/accounts`
+    fetch(ACCOUNT_URL)
+    .then(res => res.json())
+    .then(json => {
+        json.forEach(account => renderAccount(account))
+    })
+}
+
