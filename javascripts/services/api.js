326f90e36093250239493992a672146bcc5241f7
rhday <54642787+rhday@users.noreply.github.com>
2021-01-20 15:18:09 +0000

    added 'getters' to 'api.js'

--- javascripts/services/api.js
+++ javascripts/services/api.js
@@ -1,6 +1,17 @@
 class API{
+    //Constructor//
     constructor(port = 3000){
         this.url = `http://localhost:${port}`
     }
+
+    //Getters//
+    get accountURL() {
+        return this.url + 'api/v1/accounts'
+    }
+    get transactionURL() {
+        return this.url + 'api/v1/transactions'
+    }
+
+    //Fetch Requests//
+    
 }
-end 
