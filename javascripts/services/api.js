df8fb5d50446613be5596e6f9ee5e590f2d01c3a
rhday <54642787+rhday@users.noreply.github.com>
2021-01-20 16:04:50 +0000

    built 'postTransaction'

--- javascripts/services/api.js
+++ javascripts/services/api.js
@@ -4,6 +4,11 @@ class API{
         this.url = `http://localhost:${port}`
     }
 
+    //Helpers//
+    parseJSON = response => response.json()
+
+    headers = {"Accepts": "application/json", "Content-Type": "application/json"}
+
     //Getters//
     get accountURL() {
         return this.url + 'api/v1/accounts'
@@ -13,5 +18,26 @@ class API{
     }
 
     //Fetch Requests//
-    
+    fetchAccounts = () => {
+        return fetch(accountURL).then(parseJSON)
+    }
+    fetchTransactions = () => {
+        return fetch(transactionURL).then(parseJSON)
+    }
+    fetchAccount = (id) => {
+        return fetch(this.accountURL + `/${id}`).then(parseJSON)
+    }
+    fetchTransaction = (id) => {
+        return fetch(this.transactionURL + `/${id}`).then(parseJSON)
+    }
+    postTransaction = (accountId) => {
+        return fetch(this.transactionURL, {
+            method: "POST",
+            headers: this.headers,
+            body: JSON.stringify({account_id: accountId})//do I need to add the fields in my schema relating to "Transaction?"
+        }).then(parseJSON)
+    }
+    deleteTransaction = (id) => {
+        return fetch(this.transactionURL + `/${id}`).then(parseJSON)
+    }
 }
