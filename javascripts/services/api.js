1cb2afb77ebb5914b6b547be1fdc61140023f3fa
rhday <54642787+rhday@users.noreply.github.com>
2021-01-21 14:03:47 +0000

    updated getters

--- javascripts/services/api.js
+++ javascripts/services/api.js
@@ -12,18 +12,18 @@ class API {
 
     //Getters//
     get accountURL() {
-        return this.url + 'api/v1/accounts'
+        return this.url + '/api/v1/accounts'
     }
     get transactionURL() {
-        return this.url + 'api/v1/transactions'
+        return this.url + '/api/v1/accounts/1/transactions'//how do I make this dynamic?
     }
 
     //Fetch Requests//
     fetchAccounts = () => {
-        return fetch(accountURL).then(this.parseJSON)
+        return fetch(this.accountURL).then(this.parseJSON)
     }
     fetchTransactions = () => {
-        return fetch(transactionURL).then(this.parseJSON)
+        return fetch(this.transactionURL).then(this.parseJSON)
     }
     fetchAccount = (id) => {
         return fetch(this.accountURL + `/${id}`).then(this.parseJSON)
@@ -35,7 +35,7 @@ class API {
         return fetch(this.transactionURL, {
             method: "POST",
             headers: this.headers,
-            body: JSON.stringify(object)//do I need to add the fields in my schema relating to "Transaction?"
+            body: JSON.stringify(object)
         }).then(this.parseJSON)
     }//postTransaction(object){
     // object = account_id, amount, kind, date, description
