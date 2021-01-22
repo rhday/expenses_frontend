a0c5a2ac828df16565095d018faf7a9d2b596ee5
rhday <54642787+rhday@users.noreply.github.com>
2021-01-22 15:29:25 +0000

    added 'constructor' to 'transaction.js

--- javascripts/models/transaction.js
+++ javascripts/models/transaction.js
@@ -1,5 +1,14 @@
-class Transaction{ //account_id, amount, kind, date, description
-    constructor() {
-
+class Transaction{ //account_id, amount, kind, date, description ///does this need a transaction ID too?
+    constructor(data) {
+        this.data = data.id
+        this.account_id = data.account_id
+        this.amount = data.amount
+        this.kind = data.kind
+        this.date = data.date
+        this.description = data.description
+    }
+    
+    delete = () => {
+            
     }
 }
