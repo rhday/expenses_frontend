9491f1b01fca50ece64fecd4916e82d99008a9a7
rhday <54642787+rhday@users.noreply.github.com>
2021-01-26 16:32:28 +0000

    added balance to card and started building the createTransaction function

--- javascripts/models/account.js
+++ javascripts/models/account.js
@@ -12,8 +12,11 @@ class Account{ //name, balance and transactions
         })
     }
 
-    createTransaction = () => {
-        api.postTransaction(this.id).then(traxData => new Transaction(traxData))
+    createTransaction = (api) => {
+        this.kindSelect 
+        api.postTransaction(this.id).then(traxData => {
+        const newTrax = new Transaction(traxData)  
+        })/// do I need to make an "account-card.js" like in your video to be able to then add new transactions? How can I make user input fields appear to then link to the transaction button?
     }
 
     renderAccount = (data) => {
@@ -21,17 +24,34 @@ class Account{ //name, balance and transactions
     const p = document.createElement("p")
     const button = document.createElement("button")
     const ul = document.createElement("ul")
+    const balance = document.createElement("p")
+    this.amountInput = document.createElement("INPUT")
+    this.amountInput.type = "number"
+    this.kindSelect = document.createElement("SELECT")
+    const withdrawOption = document.createElement("OPTION")
+    withdrawOption.innerHTML = "Withdraw"
+    withdrawOption.value = "withdraw"
+    const depositOption = document.createElement("OPTION")
+    depositOption.innerHTML = "Deposit"
+    depositOption.value = "deposit"
 
     div.setAttribute("class", "card")
     div.setAttribute("data-id", this.id)
     p.innerHTML = this.name
+    balance.innerHTML = this.balance
     button.setAttribute("data-account-id", this.id)
     button.innerHTML = "Add Transaction"
     button.addEventListener("click", this.createTransaction)
 
     div.appendChild(p)
+    div.appendChild(this.amountInput)
+    div.appendChild(this.kindSelect)
+    this.kindSelect.appendChild(withdrawOption)
+    this.kindSelect.appendChild(depositOption)
     div.appendChild(button)
     div.appendChild(ul)
+    div.appendChild(balance)
+    
 
     main.appendChild(div)
     //this.transactions.forEach(transaction => renderTransaction(transaction))
--- javascripts/models/transaction.js
+++ javascripts/models/transaction.js
@@ -3,7 +3,7 @@ class Transaction{ //account_id, amount, kind, date, description ///does this ne
     constructor(data) {
         this.data = data.id
         this.account_id = data.account_id
-        this.amount = data.amount
+        this.amount = data.amount//want to display this on the card
         this.kind = data.kind
         this.date = data.date
         this.description = data.description
@@ -15,7 +15,7 @@ class Transaction{ //account_id, amount, kind, date, description ///does this ne
         const button = document.createElement("button")
 
         li.innerHTML = `${this.amount} (${this.kind})`
-        button.setAttribute("class", "release")
+        button.setAttribute("class", "delete")
         button.setAttribute("data-this-id", this.id)
         button.innerHTML = "Delete!"
         button.addEventListener("click", this.delete)
